aa9247227f91213c36b64b36fef2b434abb6c0b5
Hafsteinn Fannar Ragnarsson <haffi13@gmail.com>
2018-11-22 11:51:49 +0100

    Added a Token class

---
+++ DementiaProject_Two/Models/Token.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DementiaProject_Two.Models
+{
+    public class Token
+    {
+    }
+}
