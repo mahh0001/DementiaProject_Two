e1ea46014218a213316a542e843c3b9a45882340
Daniel <dani7354@edu.eal.dk>
2018-11-22 08:04:51 +0100

    Startup.cs: added configuration property

--- DementiaProject_Two/Startup.cs
+++ DementiaProject_Two/Startup.cs
@@ -5,17 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DementiaProject_Two
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+        }
+        public IConfiguration Configuration { get; private set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+           
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
