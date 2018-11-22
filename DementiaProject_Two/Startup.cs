5c10c1321be4ac0ad2eeaf43d7bcba70d2773973
Mathias Stubberup Nabe-Nielsen <mahh0001@edu.eal.dk>
2018-11-22 09:41:52 +0100

    AddDbContext for IdentityContext

--- DementiaProject_Two/Startup.cs
+++ DementiaProject_Two/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DementiaProject_Two.DataContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -23,6 +25,9 @@ namespace DementiaProject_Two
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<IdentityContext>(options => options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
+
         }
 
 
