e2b75892669f3f935cbab89e631c54f13b68be36
Mathias Stubberup Nabe-Nielsen <mahh0001@edu.eal.dk>
2018-11-27 11:26:57 +0100

    Split name

--- MatchmakingService/Models/UserInfo.cs
+++ MatchmakingService/Models/UserInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace MatchmakingService.Models
 {
     public class UserInfo
     {
-        public string Name { get; set; }
+        [Key]
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public int Age { get; set; }
         public int ZipCode { get; set; }
         public string Gender { get; set; }
