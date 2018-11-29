ba929dffe6a081b4bc87652f5e943ed2efbc0cc5
Hafsteinn Fannar Ragnarsson <haffi13@gmail.com>
2018-11-29 09:13:37 +0100

    Added UserMatch class to the MatchmakingService

--- DementiaProject_Two/Controllers/AccountController.cs
+++ DementiaProject_Two/Controllers/AccountController.cs
@@ -9,7 +9,6 @@ using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Authorization;
 
 namespace DementiaProject_Two.Controllers
 {
---
+++ MatchmakingService/Models/UserMatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchmakingService.Models
+{
+    public class UserMatch
+    {
+        public long Id { get; set; }
+        public Guid User1Id { get; set; }
+        public bool User1Agree { get; set; }
+        public Guid User2Id { get; set; }
+        public bool User2Agree { get; set; }
+    }
+}
