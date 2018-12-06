482d4f998641d18c3f88398e16035a29cab38959
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-12-06 09:10:45 +0100

    Method for getting info for the logged in user

--- DementiaProject_Two/Controllers/EditController.cs
+++ DementiaProject_Two/Controllers/EditController.cs
@@ -1,47 +1,54 @@
 ﻿using DementiaProject_Two.Models.Account;
 using DementiaProject_Two.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace DementiaProject_Two.Controllers
 {
     public class EditController : Controller
     {
+        public EditController(UserManager<IdentityUser> userman)
+        {
+            Userman = userman;
+        }
         private IRepository repo;
 
         public UserInformationModel UserInfo { get; set; }
+        public UserManager<IdentityUser> Userman { get; }
 
         public EditController(IRepository repo)
         {
             this.repo = repo;
         }
-
+        [HttpGet("")]
         public IActionResult Index()
         {
-            return View();
-        }
+            var user = Userman.FindByEmailAsync(User.Identity.Name).Result;
+            var userInformation = repo.Get(new Guid(user.Id));
 
-        public ActionResult EditProfil(UserInformationModel userModel)
-        {
-            UserInfo = repo.Get(userModel.Id);
-            if (UserInfo == null)
+            if(userInformation == null)
             {
-                return RedirectToAction("Index", "Home");
+                userInformation = new UserInformationModel() { Id = new Guid(user.Id) };
             }
-            return View();
+            return View(userInformation);
+        }
+        [HttpPost]
+        public IActionResult Update([FromForm]UserInformationModel userModel)
+        {
+           if(userModel == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            repo.Update(userModel);
+            return RedirectToAction("Index");
         }
 
-        //public IActionResult OnPost()
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        repo.Update(UserInfo);
-        //        return RedirectToAction("Index", "Home");
-        //    }
-        //    else
-        //    {
-        //        return View();
-        //    }
-        //}
+      
     }
 }
