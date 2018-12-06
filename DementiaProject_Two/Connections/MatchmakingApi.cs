9f011754c69b5058776927051c5349584c1a522a
Hafsteinn Fannar Ragnarsson <haffi13@gmail.com>
2018-12-06 11:34:10 +0100

    Created a DTO to pass the Match information from the MVC to the matchmaking service, so the results can be saved. Halfway implemented.

--- DementiaProject_Two/Connections/MatchmakingApi.cs
+++ DementiaProject_Two/Connections/MatchmakingApi.cs
@@ -1,4 +1,5 @@
 ﻿using DementiaProject_Two.Models.Account;
+using DementiaProject_Two.Models.DataTransferObjects;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -30,6 +31,18 @@ namespace DementiaProject_Two.Connections
 
             }
             return user;
+        }
+
+        public static async Task<bool> SaveMatchSelection(Guid currentUser, Guid otherUser, bool match)
+        {
+            ConfigureClient();
+            MatchDTO matchDto = new MatchDTO
+            {
+                User1 = currentUser,
+                User2 = otherUser,
+                Match = match
+            };
+            HttpResponseMessage response = await client.PostAsJsonAsync(@"api/match/savematch", matchDto);
         }
 
     }
---
+++ MatchmakingService/Models/DataTransferObjects/MatchDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchmakingService.Models.DataTransferObjects
+{
+    public class MatchDTO
+    {
+        public Guid User1 { get; set; }
+        public Guid User2 { get; set; }
+        public bool Match { get; set; }
+    }
+}
