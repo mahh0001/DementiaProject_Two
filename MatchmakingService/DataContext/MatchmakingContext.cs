0e6be8099be968a1e85536c4c64b057a6e5b874a
Mathias Stubberup Nabe-Nielsen <mahh0001@edu.eal.dk>
2018-12-06 09:00:10 +0100

    Added fake data på Matchmaking Context, lavede endpoint/metode til at få fat i alle brugere

--- MatchmakingService/DataContext/MatchmakingContext.cs
+++ MatchmakingService/DataContext/MatchmakingContext.cs
@@ -10,7 +10,10 @@ namespace MatchmakingService.DataContext
         {
             Database.EnsureCreated();
             UserInfos.Add(new UserInfo { Age = 20, FirstName = "Daniel", LastName = "Stuhr", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("9c10943b-b408-4200-b3a2-6fa2a5d96df8") });
-
+            UserInfos.Add(new UserInfo { Age = 22, FirstName = "Rasmus", LastName = "Bak", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("9c10943b-b408-8300-b3a2-6fa2a5d96df8")});
+            UserInfos.Add(new UserInfo { Age = 35, FirstName = "Jesper", LastName = "Madsen", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("8c20943b-b408-8300-b3a2-6fa2a5d96df8")});
+            UserInfos.Add(new UserInfo { Age = 24, FirstName = "Hafsteinn", LastName = "Ragnarsson", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("8c20947c-b408-8300-b3a2-6fa2a5d96df8")});
+            UserInfos.Add(new UserInfo { Age = 25, FirstName = "Mathias", LastName = "Nabe", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("8c20947c-b408-8300-b3a2-6fa2a5d96df1") });
             SaveChanges();
         }
         public DbSet<UserInfo> UserInfos { get; set; }
--- MatchmakingService/Services/Repositories/UserMatchRepository.cs
+++ MatchmakingService/Services/Repositories/UserMatchRepository.cs
@@ -23,26 +23,26 @@ namespace MatchmakingService.Services.Repositories
         }
 
         // Skroll first through
-        public UserInfo FindRandomUser(Guid userId)
-        {
-            List<UserInfo> allUsers = MatchmakingContext.UserInfos
-                .Include(x => x.Matches)
-                .Where(x => x.IdentityFK != userId)
-                .ToList();
-            foreach (var user in allUsers) {
-                // removes users from list, if user dosn't wan't to match or if both users already matched.
-                if ( 
-                    (user.Matches.Where(x => x.User2Id == userId && x.FirstSelection == false).Count() == 1) ||
-                    (user.Matches.Where(x => x.User2Id == userId && x.IsAMatch == true).Count() == 1)
-                    )
-                {
-                    allUsers.Remove(user);
-                }
-            }
+        //public UserInfo FindRandomUser(Guid userId)
+        //{
+        //    List<UserInfo> allUsers = MatchmakingContext.UserInfos
+        //        .Include(x => x.Matches)
+        //        .Where(x => x.IdentityFK != userId)
+        //        .ToList();
+        //    foreach (var user in allUsers) {
+        //        // removes users from list, if user dosn't wan't to match or if both users already matched.
+        //        if ( 
+        //            (user.Matches.Where(x => x.User2Id == userId && x.FirstSelection == false).Count() == 1) ||
+        //            (user.Matches.Where(x => x.User2Id == userId && x.IsAMatch == true).Count() == 1)
+        //            )
+        //        {
+        //            allUsers.Remove(user);
+        //        }
+        //    }
 
-            //// needs to do some kind of random selection on the allUsers and return that UserInfo
+        //    //// needs to do some kind of random selection on the allUsers and return that UserInfo
 
-            return null;
-        }
+        //    return null;
+        //}
     }
 }
