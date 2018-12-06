91b490b7849336aace295aed2fd946f6cc4fad9f
Hafsteinn Fannar Ragnarsson <haffi13@gmail.com>
2018-12-06 18:54:21 +0100

    Changed the data annotations in the registration model so the RepeatPassword property is instead displayed as "Confirm Password"

--- DementiaProject_Two/Models/Account/Registration.cs
+++ DementiaProject_Two/Models/Account/Registration.cs
@@ -13,6 +13,7 @@ namespace DementiaProject_Two.Models.Account
         public string Password { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string RepeatPassword { get; set; }
     }
