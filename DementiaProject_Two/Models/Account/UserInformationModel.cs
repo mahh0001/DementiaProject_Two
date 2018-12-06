01e098bc8444e744ba3ec29ce78166e824da8c24
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-12-06 08:52:05 +0100

    Guid as Id

--- DementiaProject_Two/Models/Account/UserInformationModel.cs
+++ DementiaProject_Two/Models/Account/UserInformationModel.cs
@@ -14,6 +14,6 @@ namespace DementiaProject_Two.Models.Account
         public byte[] Picture { get; set; }
         //Fremmednøgle til Identity
         [Key]
-        public Guid IdentityFK { get; set; }
+        public Guid Id { get; set; }
     }
 }
