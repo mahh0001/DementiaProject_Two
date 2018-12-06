fcda4ee086b1363d16b0b951529eb78cfb1c24b5
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-12-06 08:47:34 +0100

    UserInfomodel

--- DementiaProject_Two/Models/Account/UserInformationModel.cs
+++ DementiaProject_Two/Models/Account/UserInformationModel.cs
@@ -1,13 +1,19 @@
-﻿namespace DementiaProject_Two.Models.Account
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DementiaProject_Two.Models.Account
 {
     public class UserInformationModel
     {
-        public int Id { get; set; }
+     
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
         public int ZipCode { get; set; }
         public string Gender { get; set; }
         public byte[] Picture { get; set; }
+        //Fremmednøgle til Identity
+        [Key]
+        public Guid IdentityFK { get; set; }
     }
 }
