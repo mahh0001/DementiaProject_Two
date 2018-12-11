284de23a62433c44adc81b3964a36a779a3e4314
Jesper Albrecht Madsen <jesp209i@edu.eal.dk>
2018-12-11 13:08:16 +0100

    d

--- MatchmakingService/DataContext/MatchmakingContext.cs
+++ MatchmakingService/DataContext/MatchmakingContext.cs
@@ -9,14 +9,42 @@ namespace MatchmakingService.DataContext
         public MatchmakingContext(DbContextOptions<MatchmakingContext> options) : base(options)
         {
             Database.EnsureCreated();
-            UserInfos.Add(new UserInfo { Age = 20, FirstName = "Daniel", LastName = "Stuhr", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("9c10943b-b408-4200-b3a2-6fa2a5d96df8") });
-            UserInfos.Add(new UserInfo { Age = 22, FirstName = "Rasmus", LastName = "Bak", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("9c10943b-b408-8300-b3a2-6fa2a5d96df8")});
-            UserInfos.Add(new UserInfo { Age = 36, FirstName = "Jesper", LastName = "Madsen", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("8c20943b-b408-8300-b3a2-6fa2a5d96df8")});
-            UserInfos.Add(new UserInfo { Age = 24, FirstName = "Hafsteinn", LastName = "Ragnarsson", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("8c20947c-b408-8300-b3a2-6fa2a5d96df8")});
-            UserInfos.Add(new UserInfo { Age = 25, FirstName = "Mathias", LastName = "Nabe", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("8c20947c-b408-8300-b3a2-6fa2a5d96df1") });
-            SaveChanges();
+            if (UserInfos.CountAsync().Result == 0)
+            {
+                UserInfos.Add(new UserInfo { Age = 20, FirstName = "Daniel", LastName = "Stuhr", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("9c10943b-b408-4200-b3a2-6fa2a5d96df8") });
+                UserInfos.Add(new UserInfo { Age = 22, FirstName = "Rasmus", LastName = "Bak", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("9c10943b-b408-8300-b3a2-6fa2a5d96df8") });
+                UserInfos.Add(new UserInfo { Age = 36, FirstName = "Jesper", LastName = "Madsen", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("8c20943b-b408-8300-b3a2-6fa2a5d96df8") });
+                UserInfos.Add(new UserInfo { Age = 24, FirstName = "Hafsteinn", LastName = "Ragnarsson", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("8c20947c-b408-8300-b3a2-6fa2a5d96df8") });
+                UserInfos.Add(new UserInfo { Age = 25, FirstName = "Mathias", LastName = "Nabe", Gender = "Male", ZipCode = 5000, IdentityFK = Guid.Parse("8c20947c-b408-8300-b3a2-6fa2a5d96df1") });
+                SaveChanges();
+            }
         }
         public DbSet<UserInfo> UserInfos { get; set; }
         public DbSet<UserMatch> Matches { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserInfo>()
+                .HasMany(user => user.Matches);
+            modelBuilder.Entity<UserMatch>()
+                .HasOne(x => x.UserInfo)
+                .WithMany(x => x.Matches)
+                .HasForeignKey(x => x.User2Id);
+        }
+
+
+    //    modelBuilder.Entity<PostTag>()
+    //        .HasKey(t => new { t.PostId, t.TagId
+    //});
+
+    //    modelBuilder.Entity<PostTag>()
+    //        .HasOne(pt => pt.Post)
+    //        .WithMany(p => p.PostTags)
+    //        .HasForeignKey(pt => pt.PostId);
+
+    //modelBuilder.Entity<PostTag>()
+    //        .HasOne(pt => pt.Tag)
+    //        .WithMany(t => t.PostTags)
+    //        .HasForeignKey(pt => pt.TagId);
     }
 }
--- MatchmakingService/Models/UserMatch.cs
+++ MatchmakingService/Models/UserMatch.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,11 @@ namespace MatchmakingService.Models
     public class UserMatch
     {
         [Key]
-        public Guid Id { get; set; }
-        public Guid User1Id { get; set; }
-        public virtual UserInfo User1 { get; set; }
-        public Guid User2Id { get; set; }
+        public int Id { get; set; }
+        public long UserInfoId { get; set; }
+        [ForeignKey("UserInfoId")]
+        public virtual UserInfo UserInfo { get; set; }
+        public long User2Id { get; set; }
         public virtual UserInfo User2 { get; set; }
         public bool FirstSelection { get; set; }
         public bool? IsAMatch { get; set; }
