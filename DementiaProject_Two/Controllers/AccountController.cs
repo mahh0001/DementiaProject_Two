c5bd69ef413ebab7571e13f677388135532f6671
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-12-13 08:39:33 +0100

    Match mock

--- DementiaProject_Two/Controllers/AccountController.cs
+++ DementiaProject_Two/Controllers/AccountController.cs
@@ -72,6 +72,7 @@ namespace DementiaProject_Two.Controllers
         }
 
         [HttpPost("Login")]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(Login user, string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
